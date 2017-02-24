a11d2ce8b93aea8b961dc65661aae86293a8739a
Octavio Cedeno <ocedeno@Octavios-MacBook-Pro.local>
2017-02-24 09:55:42 -0500

    Added: -Attached VC classes to UI -Added DataModel object
    Next:
    -Create a pin by tap/hold

---
+++ Virtual Tourist/MapViewController.swift
@@ -0,0 +1,24 @@
+//
+//  MapViewController.swift
+//  Virtual Tourist
+//
+//  Created by Octavio Cedeno on 2/24/17.
+//  Copyright © 2017 Cedeno Enterprises, Inc. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController
+{
+    //IBOutlets:
+    @IBOutlet weak var mapView: MKMapView!
+    
+    //Variables/Constants
+    
+    //Life Cycle:
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+}
---
+++ Virtual Tourist/PhotoAlbumViewController.swift
@@ -0,0 +1,26 @@
+//
+//  PhotoAlbumViewController.swift
+//  Virtual Tourist
+//
+//  Created by Octavio Cedeno on 2/24/17.
+//  Copyright © 2017 Cedeno Enterprises, Inc. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class PhotoAlbumViewController: UIViewController, UICollectionViewDelegate
+{
+
+    //IBOutlets:
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var flickrCollectionView: UICollectionView!
+    
+    //Life Cycle:
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+    
+
+}
